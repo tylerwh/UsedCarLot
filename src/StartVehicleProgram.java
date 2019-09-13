d28d28585e70c14e697cb171841959b3a1a51fa1
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-12 22:47:43 -0500

    Add comments and reorganize code

--- src/StartVehicleProgram.java
+++ src/StartVehicleProgram.java
@@ -5,14 +5,18 @@ import controller.VehicleHelper;
 import model.Vehicle;
 
 public class StartVehicleProgram {
-	
+
 	static Scanner in = new Scanner(System.in);
 	static VehicleHelper veh = new VehicleHelper();
-	
+
+	// Main method with the menuRun() method
 	public static void main(String[] args) {
 		menuRun();
 	}
-	
+
+	// This is the method that will act as our user interface
+	// To add, edit, and remove vehicles, as well as view all vehicles
+	// from the carshop database
 	public static void menuRun() {
 		boolean runAgain = true;
 		System.out.println("------- Welcome to the Used Car Lot! -------");
@@ -26,8 +30,9 @@ public class StartVehicleProgram {
 			System.out.print("*  Your selection: ");
 			int selection = in.nextInt();
 			in.nextLine();
-			
+
 			// Send user's selection to appropriate method
+			// contained within this class file
 			if (selection == 1) {
 				addVehicle();
 			} else if (selection == 2) {
@@ -43,33 +48,28 @@ public class StartVehicleProgram {
 				System.out.println("|*****************************|");
 				runAgain = false;
 			}
-			
+
 		}
 	}
 
-	private static void viewInventory() {
+	// Selection 1 - This method allows user to add a new vehicle to the inventory table
+	private static void addVehicle() {
 		// TODO Auto-generated method stub
-		List<Vehicle> allVehicles = veh.showInventory();
-		for(Vehicle singleVehicle : allVehicles) {
-			System.out.println(singleVehicle.returnVechicleDetails());
-		}
-		
-	}
-
-	private static void deleteVehicle() {
-		// TODO Auto-generated method stub
-		System.out.println("Vehicle's make: ");
+		System.out.println("Make: ");
 		String make = in.nextLine();
-		System.out.println("Vehicle's model: ");
+		System.out.println("Model: ");
 		String model = in.nextLine();
-		System.out.println("Vehicle's year: ");
+		System.out.println("Year: ");
 		int year = in.nextInt();
-		System.out.println("Vehicle's price: ");
+		System.out.println("Price: ");
 		double price = in.nextDouble();
-		Vehicle toRemove = new Vehicle(make, model, year, price);
-		veh.removeVehicle(toRemove);
+		
+		// Create Vehicle object to pass into insertVehicle()
+		Vehicle toAdd = new Vehicle(model, make, year, price);
+		veh.insertVehicle(toAdd);
 	}
 
+	// Selection 2 - This method will allow user to find an existing vehicle in inventory table and edit any of the attributes (except for the ID)
 	private static void editVehicle() {
 		// TODO Auto-generated method stub
 		System.out.println("Let's locate the vehicle you would like to edit!");
@@ -77,15 +77,17 @@ public class StartVehicleProgram {
 		String model = in.nextLine();
 		List<Vehicle> vehiclesFound;
 		vehiclesFound = veh.findVehiclesByModel(model);
-		
-		if(!vehiclesFound.isEmpty()) {
+
+		if (!vehiclesFound.isEmpty()) {
 			System.out.println("I think we found 'em!");
-			for(Vehicle v : vehiclesFound) {
+			for (Vehicle v : vehiclesFound) {
 				System.out.println(v.getId() + " : " + v.toString());
 			}
+			
+			// The "ID" requested below actually pertains to the ID retrieved from the inventory table
 			System.out.println("\nWhich ID would you like to edit? ");
 			int id = in.nextInt();
-			
+
 			Vehicle editedVehicle = veh.searchForVehicleById(id);
 			System.out.println("Retrieved: " + editedVehicle.returnVechicleDetails());
 			System.out.println("\nWhat would you like to update? ");
@@ -94,7 +96,7 @@ public class StartVehicleProgram {
 			System.out.println("3 : Year");
 			System.out.println("4 : Price");
 			int selection = in.nextInt();
-			
+
 			if (selection == 1) {
 				System.out.println("New Make: ");
 				String editMake = in.nextLine();
@@ -112,26 +114,38 @@ public class StartVehicleProgram {
 				double editPrice = in.nextDouble();
 				editedVehicle.setPrice(editPrice);
 			}
-			
+
 			veh.updateVehicle(editedVehicle);
-			
+
 		} else {
 			System.out.println("\nSorry, Charlie! No vehicle here..\n");
 		}
 	}
 
-	private static void addVehicle() {
+	// Selection 3 - This method will prompt user to search for desired vehicle
+	// in inventory table and delete it.
+	private static void deleteVehicle() {
 		// TODO Auto-generated method stub
-		System.out.println("Model: ");
-		String model = in.nextLine();
-		System.out.println("Make: ");
+		System.out.println("Vehicle's make: ");
 		String make = in.nextLine();
-		System.out.println("Year: ");
+		System.out.println("Vehicle's model: ");
+		String model = in.nextLine();
+		System.out.println("Vehicle's year: ");
 		int year = in.nextInt();
-		System.out.println("Price: ");
+		System.out.println("Vehicle's price: ");
 		double price = in.nextDouble();
-		Vehicle toAdd = new Vehicle(model, make, year, price);
-		veh.insertVehicle(toAdd);
+		Vehicle toRemove = new Vehicle(make, model, year, price);
+		veh.removeVehicle(toRemove);
+	}
+
+	// Selection 4 - Allows user to view the current inventory
+	private static void viewInventory() {
+		// TODO Auto-generated method stub
+		List<Vehicle> allVehicles = veh.showInventory();
+		for (Vehicle singleVehicle : allVehicles) {
+			System.out.println(singleVehicle.returnVechicleDetails());
+		}
+
 	}
 
 }
